b626df9012303e9bae89fb132d077c486d77fedf
roman1970 <r0man4ernyshev@gmail.com>
2015-09-03 17:53:41 +0600

    htaccessfix

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,9 +1,37 @@
 <?php
 
+if(!isset($dbName))
+{
+    switch (__DIR__)
+    {
+        case '/home/romanych/public_html/plis/basic/config':
+            $dbName = 'plis';
+            $dbUser = 'root';
+            $dbPassword = 'vbifcdtnf';
+            $dbHost = 'localhost';
+            break;
+
+        case '/home/virtwww/w_qplis-ru_c772050d/http/lis/basic/config':
+            $dbName = 'gb_x_qplis';
+            $dbUser = 'gb_x_qplis';
+            $dbPassword = '9z49c9ezgh';
+            $dbHost = 'mysql92.1gb.ru';
+            break;
+
+
+        default:
+            $dbName = '';
+            $dbUser = '';
+            $dbPassword = '';
+            $dbHost = '';
+            break;
+    }
+}
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=plis',
-    'username' => 'root',
-    'password' => 'vbifcdtnf',
+    'dsn' => 'mysql:host='.$dbHost.';dbname='.$dbName,
+    'username' => $dbUser,
+    'password' => $dbPassword,
     'charset' => 'utf8',
 ];
