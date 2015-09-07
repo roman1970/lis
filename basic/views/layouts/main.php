b34ccc3b5cc052d79eeaec75a9b69d655ae1041a
roman1970 <r0man4ernyshev@gmail.com>
2015-09-07 15:14:07 +0600

    module theme fix

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -27,7 +27,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'qpLIS',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -61,9 +61,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; qpLIS <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right">Все права защищены</p>
     </div>
 </footer>
 
