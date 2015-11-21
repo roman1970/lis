6a4cef57e6792e6724d11aa801ae76e7b0a97fff
roman1970 <r0man4ernyshev@gmail.com>
2015-11-21 18:13:25 +0600

    ball fix

--- basic/web/themes/russia2018/js/main.js
+++ basic/web/themes/russia2018/js/main.js
@@ -10,10 +10,25 @@ $(function(){
     /*image.onload = function(){alert('картинка существует')}; //проверка существование картинки*/
     /*image.onerror = function(){alert('картинка не существует')}; //проверка существование картинки*/
 
+    // загружаем изображение тени и бликов планеты
+    var fxShadow = new Image();
+    fxShadow.src = 'themes/russia2018/images/sphere.png';
+
 
     // определяем длину и высоту элемента canvas
     var width = $('#'+pl_id).width();
     var height = $('#'+pl_id).height();
+
+    // рассчитываем угол вращения планеты
+    var beta = 360/900;
+    var beta = (beta*Math.PI)/360;
+    var l = (Math.sqrt(width*width+width*width))/2;
+    var gam = Math.PI - ((Math.PI - (beta * Math.PI)/360)/2) - (Math.PI/4);
+    var b = 2*l*Math.sin(beta/2);
+    var x = b*Math.sin(gam);
+    var y = b*Math.cos(gam);
+    var p1 = Math.cos(beta);
+    var p2 = Math.sin(beta);
 
     var canvas = document.getElementById(pl_id);
     var id = canvas.getContext("2d");
@@ -24,10 +39,14 @@ $(function(){
     var drawPl = function(){
 
         id.clearRect(0, 0, width, height);
+        // применяем к нашей планете вращение
+        id.transform(p1, p2, -p2, p1, x, -y);
         // рисуем карту с новыми координатами внутри элемента
         id.drawImage(image, newMoveWidth, newMoveHeight, width, height, 0, 0, width, height);
-
-        if (newMoveWidth >= 899.5) newMoveWidth = 0; // если смещение достигло предела, начинаем сначала
+        //добавляем тень и блики
+        id.drawImage(fxShadow, 0, 0, width, height);
+        // если смещение достигло предела, начинаем сначала
+        if (newMoveWidth >= 899.5) newMoveWidth = 0;
         else newMoveWidth = newMoveWidth+0.5; // иначе двигаем карту дальше
 
     }
