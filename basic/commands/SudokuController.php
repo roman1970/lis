04431eb45b43855f64eb66077c9484298da6ee9d
roman1970 <r0man4ernyshev@gmail.com>
2016-06-12 17:12:26 +0600

    test fix

--- basic/commands/SudokuController.php
+++ basic/commands/SudokuController.php
@@ -26,37 +26,110 @@ class SudokuController extends Controller
 
     public function actionFillField($input){
         $arr = [];
+        $non_allowed = [];
+        $kvadrat = 0;
         $j = 0;
+        $sudoku = "";
         //рисуем и заполняем массив
         for($i = 0; $i <= 80; $i++){
 
-            $arr[$i % 9][$j] = $input[$i];
+
             if($i % 9 == 0) {
                 $j++;
                 echo "\r\n";
+                $sudoku .= "\r\n";
             }
+            $arr[$i % 9][$j] = $input[$i];
 
             echo $input[$i] . " | ";
+            $sudoku .= $input[$i] . " | ";
 
         }
         echo "\r\n";
         //print_r($arr);
 
-        for($i=0; $i <= 8; $i++){
-            for($m=1; $m < 9; $m++){
-                if($arr[$i][$m] == '1') {
-                    echo "В столбец ". $i . " в строку " . $m . " единицу ставить нельзя";
-                }
-            }
-        }
+        for($c=1; $c < 10; $c++){
 
-        for($i=0; $i < 9; $i += 3){
-            for($m=1; $m <= 3; $m++){
-                if($arr[$i][$m] == '1') {
-                    echo "В столбец ". $i . " в строку " . $m . " единицу ставить нельзя";
+
+            //проверяем строки и столбцы
+            for($i=0; $i <= 8; $i++){
+                for($m=1; $m < 9; $m++){
+                    if($arr[$i][$m] == $c) {
+                        echo "\r\n";
+                        echo "В столбец ". $i . " в строку " . $m ." " . $c ."  ставить нельзя";
+                        for($m=1; $m < 9; $m++) {
+                            $non_allowed[$c][$i][$m] = 0;
+                        }
+                        break;
+                    }
+
                 }
             }
+            //print_r($non_allowed);
+            //echo $sudoku;
+            //exit;
+
+            //проверяем квадраты
+            for($n=0; $n < 9; $n+=3) {
+
+                for($i=0; $i < 3; $i++){
+                    for($m=1; $m <= 3; $m++){
+                        //print_r($arr);
+                        if($arr[$i][$m+$n] == $c) {
+                            $kvadrat = $n+1;
+                            echo "\r\n";
+                            echo "В квадрат ". $kvadrat ." " . $c ." ставить нельзя";
+                            for($i=0; $i < 3; $i++){
+                                for($m=1; $m <= 3; $m++) {
+                                    $non_allowed[$c][$i][$m+$n] = 0;
+                                }
+                            }
+                            //$non_allowed[$c]['q'][] = $kvadrat;
+                            break;
+                        }
+                    }
+                }
+
+                for($i=3; $i < 6; $i++){
+                    for($m=1; $m <= 3; $m++){
+                        if($arr[$i][$m+$n] == $c) {
+                            $kvadrat = $n+2;
+                            echo "\r\n";
+                            echo "В квадрат ". $kvadrat ." " . $c ." ставить нельзя";
+                            for($i=3; $i < 6; $i++){
+                                for($m=1; $m <= 3; $m++){
+                                    $non_allowed[$c][$i][$m+$n] = 0;
+                                }
+                            }
+                            //$non_allowed[$c]['q'][] = $kvadrat;
+                            break;
+                        }
+                    }
+                }
+
+                for($i=6; $i < 9; $i++){
+                    for($m=1; $m <= 3; $m++){
+                        if($arr[$i][$m+$n] == $c) {
+                            $kvadrat = $n+3;
+                            echo "\r\n";
+                            echo "В квадрат ". $kvadrat ." " . $c ." ставить нельзя";
+                            for($i=6; $i < 9; $i++){
+                                for($m=1; $m <= 3; $m++){
+                                    $non_allowed[$c][$i][$m+$n] = 0;
+                                }
+                            }
+
+                            //$non_allowed[$c]['q'][] = $kvadrat;
+                            break;
+                        }
+                    }
+                }
+            }
+
+
         }
+        print_r($non_allowed);
+        echo $sudoku;
 
 
     }
