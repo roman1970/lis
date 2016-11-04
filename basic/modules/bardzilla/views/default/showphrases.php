d4c219dc3bbbec2daecbd8e6a76436774c5cdc14
roman1970 <r0man4ernyshev@gmail.com>
2016-11-04 17:23:50 +0700

    regular

--- basic/modules/bardzilla/views/default/showphrases.php
+++ basic/modules/bardzilla/views/default/showphrases.php
@@ -2,26 +2,26 @@
     var bounce = new Bounce();
     bounce
         .translate({
-            from: { x: -300, y: 0 },
+            from: { x: -100, y: 0 },
             to: { x: 0, y: 0 },
-            duration: 600,
+            duration: 100,
             stiffness: 4
         })
         .scale({
             from: { x: 1, y: 1 },
-            to: { x: 0.1, y: 2.3 },
+            to: { x: 0.1, y: 1.3 },
             easing: "sway",
-            duration: 800,
+            duration: 100,
             delay: 65,
             stiffness: 2
         })
-        .scale({
+        /*.scale({
             from: { x: 1, y: 1},
-            to: { x: 5, y: 1 },
+            to: { x: 2, y: 1 },
             easing: "sway",
             duration: 300,
             delay: 30,
-        })
+        })*/
     bounce.applyTo(document.querySelectorAll(".phrasa"));
     // or with jQuery: bounce.applyTo($(".animation-target"));
 </script>
