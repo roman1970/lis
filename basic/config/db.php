1e308f5e7c023528a5bbea1f6fd976a325e7654d
roman1970 <r0man4ernyshev@gmail.com>
2016-11-21 15:08:00 +0700

    regular

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,47 +1,5 @@
 <?php
 
-if(!isset($dbName))
-{
-    switch (__DIR__)
-    {
-        case '/home/romanych/public_html/plis/basic/config':
-            $dbName = $params['db_name'];
-            $dbUser = $params['db_username'];
-            $dbPassword = $params['db_password'];
-            $dbHost = $params['db_host'];
-            break;
-        /*
-         *  'dsn' => 'mysql:host='.$params['db_host'].';dbname='.$params['db_name'],
-    'username' => $params['db_username'],
-    'password' => $params['db_password'],
-
-        case '/home/romanych/public_html/plis/basic/config':
-            $dbName = '';
-            $dbUser = '';
-            $dbPassword = '';
-            $dbHost = '127.0.0.1';
-            break;
-
-
-
-        */
-
-        case '/home/virtwww/w_qplis-ru_c772050d/http/lis/basic/config':
-            $dbName = 'gb_x_qplis';
-            $dbUser = 'gb_x_qplis';
-            $dbPassword = '9z49c9ezgh';
-            $dbHost = 'mysql92.1gb.ru';
-            break;
-
-
-        default:
-            $dbName = '';
-            $dbUser = '';
-            $dbPassword = '';
-            $dbHost = '';
-            break;
-    }
-}
 
 return [
     'class' => 'yii\db\Connection',
