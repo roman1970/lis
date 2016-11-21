d378d2765e870ba842604a83002c8a2c61a80775
roman1970 <r0man4ernyshev@gmail.com>
2016-11-21 10:05:22 +0700

    del params

--- basic/config/db.php
+++ basic/config/db.php
@@ -23,7 +23,18 @@ if(!isset($dbName))
             break;
 
 
-
+return [
+    //'log_file' =>  '/var/log/nginx/access.log',
+    'email_host' => 'smtp-20.1gb.ru',
+    'cookieValidationKey' => '7haKvarJ7EWfIVOQeT6CaP_Am-8B1ZmL',
+    'mail_username' => 'u424229',
+    'mail_password' => '280699b4gh',
+    'api_key' => 'e44',
+    'db_host' => 'localhost',
+    'db_name' => 'plis',
+    'db_username' => 'root',
+    'db_password' => 'vbifcdtnf'
+];
         */
 
         case '/home/virtwww/w_qplis-ru_c772050d/http/lis/basic/config':
