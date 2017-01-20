460963d181238d86778326d26278832bf71dea68
roman1970 <r0man4ernyshev@gmail.com>
2017-01-20 09:33:31 +0700

    regular

--- basic/migrations/m160820_050241_add_act_id_to_article.php
+++ basic/migrations/m160820_050241_add_act_id_to_article.php
@@ -7,11 +7,11 @@ class m160820_050241_add_act_id_to_article extends Migration
     public function up()
     {
 
-        $this->addColumn('{{qparticles}}', 'act_id', 'INT(4)  DEFAULT 1');
+        /*$this->addColumn('{{qparticles}}', 'act_id', 'INT(4)  DEFAULT 1');
 
         $this->createIndex("ux_qparticles_act_id", 'qparticles', "act_id", false);
         $this->db->createCommand('ALTER TABLE {{qparticles}} ADD FOREIGN KEY (`act_id`) REFERENCES {{controll_acts}} (`id`) ON DELETE CASCADE ON UPDATE NO ACTION ;')->execute();
-
+         */
     }
 
     public function down()
