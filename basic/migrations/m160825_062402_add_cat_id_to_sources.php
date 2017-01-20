c324bfd98571f8392d9a9f45742337d54aae2759
roman1970 <r0man4ernyshev@gmail.com>
2017-01-20 09:40:04 +0700

    regular

--- basic/migrations/m160825_062402_add_cat_id_to_sources.php
+++ basic/migrations/m160825_062402_add_cat_id_to_sources.php
@@ -6,11 +6,11 @@ class m160825_062402_add_cat_id_to_sources extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{sources}}', 'cat_id', 'INT(4)  DEFAULT 1');
+       /* $this->addColumn('{{sources}}', 'cat_id', 'INT(4)  DEFAULT 1');
 
         $this->createIndex("ux_sources_cat_id", 'sources', "cat_id", false);
         $this->db->createCommand('ALTER TABLE {{sources}} ADD FOREIGN KEY (`cat_id`) REFERENCES {{qpcategory}} (`id`) ON DELETE CASCADE ON UPDATE NO ACTION ;')->execute();
-
+       */
     }
 
     public function down()
